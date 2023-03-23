1037abd2dd04801652c8109606b352c3f3ff8f0f
s1030905 <s1030905@outlook.com>
2023-03-23 13:26:14 +0800

    add seeder

---
+++ models/seeds/urlSeeder.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+const URL = require("../urls")
+const db = mongoose.connection
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: '../../.env' })
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+db.on("error", () => {
+  console.log("mongodb connect failed")
+})
+db.once("open", () => {
+  console.log("mongodb connect success")
+  URL.create({ name: "https://www.google.com" })
+  console.log("seeder created")
+})
