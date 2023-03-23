c318481684b85e0fe9fefcc48c98faa17fb19dd7
s1030905 <s1030905@outlook.com>
2023-03-23 10:38:50 +0800

    connect mongodb

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require("express")
 const exphdbars = require("express-handlebars")
 const app = express()
-// const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
 app.set("view engine", "handlebars")
 app.engine("handlebars", exphdbars({ defaultLayout: "main" }))
 
-// if (process.env.NODE_ENV !== "production") {
-//   require("dotenv")
-// }
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-// const db = mongoose.connection
+const db = mongoose.connection
+
+db.on("error", () => {
+  console.log("mongodb failed")
+})
+db.once("open", () => {
+  console.log("mongodb success")
+})
 
 app.get("/", (req, res) => {
   res.render("index")
