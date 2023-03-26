892245b2aa604d31bac8e5d116aaccb8b4ab8e7e
s1030905 <s1030905@outlook.com>
2023-03-26 21:21:23 +0800

    url_shortener_v1

--- alert.js
+++ alert.js
@@ -1,12 +1,10 @@
-{/* <script>
-  function alertInput() {
-    const input = document.querySelector("#input")
-  const submitBtn = document.querySelector("#submitBtn")
-    submitBtn.addEventListener("click", (event) => {
-      if (!input.value) {
-    alert("Required field is blank")
-  }
-    })
-  }
-  alertInput()
-</script> */}
+// function alertInput() {
+//   const input = document.querySelector("#input")
+//   const submitBtn = document.querySelector("#submitBtn")
+//   submitBtn.addEventListener("click", (event) => {
+//     if (!input.value) {
+//       alert("Required field is blank")
+//     }
+//   })
+// }
+// alertInput()
--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -1,17 +1,15 @@
 const URL = require("../urls")
 const mongoose = require("mongoose")
 const db = mongoose.connection
+const generator = require("../../url_shortener")
 
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: 'C:/Users/8787/git/ac_exercise/url_shortener/.env' })
+  require("dotenv").config()
 }
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 db.on("error", () => {
   console.log("mongodb failed")
-})
-db.once("open", () => {
-  console.log("mongodb success")
 })
 
 db.once("open", () => {
@@ -19,14 +17,23 @@ db.once("open", () => {
 
   URL.create({ name: "https://www.google.com" })
     .then((url) => {
-      return URL.findOne({ name: url.name })
-    })
-    .then(url => {
-      const id = url._id.toString()
-      const urlId = id.slice(-6)
-      url.urlShortener = `https://ptt.cc/${urlId}`
+      const urlShortener = generator()
+      url.urlShortener = urlShortener
       return url.save()
     })
+  URL.create({ name: "https://www.w3schools.com/" })
+    .then((url) => {
+      const urlShortener = generator()
+      url.urlShortener = urlShortener
+      return url.save()
+    })
+  URL.create({ name: "https://medium.com/@wind8sky999" })
+    .then((url) => {
+      const urlShortener = generator()
+      url.urlShortener = urlShortener
+      return url.save()
+    })
+
     .catch(error => console.log(error))
 
   console.log("seeder created")
