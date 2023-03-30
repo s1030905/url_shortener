898408f6ae16db583e369d4080a620b763c09302
s1030905 <s1030905@outlook.com>
2023-03-30 22:06:56 +0800

    create mutiple seed at once by for loop

--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -2,6 +2,7 @@ const URL = require("../urls")
 const mongoose = require("mongoose")
 const db = mongoose.connection
 const generator = require("../../url_shortener")
+const seeder = [{ name: "https://www.google.com" }, { name: "https://www.w3schools.com/" }, { name: "https://medium.com/@wind8sky999" }]
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config()
@@ -14,27 +15,14 @@ db.on("error", () => {
 
 db.once("open", () => {
   console.log("mongodb connect success")
-
-  URL.create({ name: "https://www.google.com" })
-    .then((url) => {
-      const urlShortener = generator()
-      url.urlShortener = urlShortener
-      return url.save()
-    })
-  URL.create({ name: "https://www.w3schools.com/" })
-    .then((url) => {
-      const urlShortener = generator()
-      url.urlShortener = urlShortener
-      return url.save()
-    })
-  URL.create({ name: "https://medium.com/@wind8sky999" })
-    .then((url) => {
-      const urlShortener = generator()
-      url.urlShortener = urlShortener
-      return url.save()
-    })
-
-    .catch(error => console.log(error))
-
+  for (i = 0; i < seeder.length; i++) {
+    URL.create({ name: "https://www.google.com" })
+      .then((url) => {
+        const urlShortener = generator()
+        url.urlShortener = urlShortener
+        return url.save()
+      })
+      .catch(error => console.log(error))
+  }
   console.log("seeder created")
 })
